d5dc7be4c3f5fbca9c37765b3fc9ce759fdaa61d
Giovanni Ticas <giovnnitcas@gmail.com>
2023-01-09 15:06:55 -0500

    LOOPS PRACTICE
    learned about the different types of loops and their methods, will practice them in codewars

---
+++ JS-10/script.js
@@ -0,0 +1,23 @@
+// LOOPS: WHILE
+
+let myNumber = 50
+
+while (myNumber < 50) {
+    console.log(myNumber)
+    myNumber++
+}
+
+// LOOPS: DO WHILE
+
+do {
+    console.log(myNumber)
+} while (myNumber < 50)
+
+// LOOPS: FOR
+
+// let arr  = ['a','b','c']
+let name = "Giovanni"
+for (let i = 0; i < name.length; i++) {
+
+    console.log(name[i] )
+}
